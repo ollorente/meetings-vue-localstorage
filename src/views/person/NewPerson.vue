ec915e6cf2ce7b072d6a19a339b05dce34774381
Óscar Llorente <llorente_oscar@hotmail.com>
2021-03-19 19:52:00 -0500

    Se agrega NewPerson

--- src/views/person/NewPerson.vue
+++ src/views/person/NewPerson.vue
@@ -1,12 +1,88 @@
 <template>
   <div class="new-person">
-    <h1>This is a new person page</h1>
+    <h1>New person</h1>
+    <form @submit.prevent="addPerson">
+      <input
+        type="text"
+        v-model="person.name"
+        id="name"
+        placeholder="Nombre de usuario"
+      />
+      <input
+        type="email"
+        v-model="person.email"
+        id="email"
+        placeholder="email@email.com"
+      />
+      <input type="text" v-model="person.role" id="role" placeholder="Cargo" />
+      <button type="submit">Agregar</button>
+    </form>
+    <div id="alert" v-if="alert.error">
+      {{ alert.msg }}
+    </div>
   </div>
 </template>
 
 <script>
+import { db, dbName } from "@/main";
+
 export default {
   name: "NewPerson",
   components: {},
+  data() {
+    return {
+      person: {
+        name: null,
+        email: null,
+        role: null,
+      },
+      alert: {
+        error: true,
+        msg: null,
+      },
+    };
+  },
+  methods: {
+    async addPerson() {
+      const user = Object.values(db.people).filter(
+        (e) => e.email === this.person.email
+      );
+
+      if (user.length > 0) {
+        this.alert.error = true;
+        this.alert.msg = `El Correo "${this.person.email}" ya existe.`;
+
+        setTimeout(() => {
+          this.alert.error = false;
+        }, 4000);
+
+        return;
+      } else {
+        const date = Date.now();
+
+        const person = {
+          id: date,
+          name: await this.person.name,
+          email: await this.person.email,
+          photoURL: "",
+          role: await this.person.role,
+          isActive: true,
+          isLock: false,
+          createdAt: date,
+          updatedAt: date,
+        };
+
+        db.people[person.id] = person;
+
+        localStorage.setItem(dbName, JSON.stringify(db));
+
+        this.person.name = "";
+        this.person.email = "";
+        this.person.role = "";
+
+        await this.$router.replace({ name: "People" });
+      }
+    },
+  },
 };
 </script>
--- src/views/person/People.vue
+++ src/views/person/People.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="people">
     <h1>This is a people page</h1>
+    <router-link :to="{ name: 'NewPerson' }">Agregar</router-link>
   </div>
 </template>
 
