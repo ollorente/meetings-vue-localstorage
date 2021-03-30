0bac8de1095914bdcbbddd0e251f9d32653623ff
Óscar Llorente <llorente_oscar@hotmail.com>
2021-03-30 00:23:42 -0500

    Conectándolo al store

--- src/views/project/Projects.vue
+++ src/views/project/Projects.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="projects">
     <h1 style="margin: 0">
-      {{ total }} {{ total === 1 ? "Proyecto" : "Proyectos" }}
+      {{ getAllProjects.length }}
+      {{ getAllProjects.length === 1 ? "Proyecto" : "Proyectos" }}
     </h1>
     <p>
       <router-link :to="{ name: 'NewProject' }" class="link"
         >Agregar</router-link
       >
     </p>
-    <p v-for="(project, index) in projects" :key="index" class="parrafo">
+    <p v-for="(project, index) in getProjects" :key="index" class="parrafo">
       <span class="parrafo__info">
         <span class="parrafo__info__number">{{ index + 1 }}</span>
         <span class="parrafo__info__name"
@@ -22,12 +23,15 @@
       <span class="parrafo__status">{{
         project.isActive ? "Activo" : "Inactivo"
       }}</span>
+      <span class="parrafo__status">{{
+        project.isLock ? "Bloqueado" : "Público"
+      }}</span>
     </p>
   </div>
 </template>
 
 <script>
-import { db } from "@/main";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "Projects",
@@ -36,48 +40,20 @@ export default {
     return {
       limit: parseInt(this.limit || 20),
       page: parseInt(this.page) > 0 ? parseInt(this.page || 1) : 1,
-      total: 0,
-      projects: [],
     };
   },
   created() {
-    this.getProjects();
-    this.getTotalProjects();
+    this.fetchProjects({ limit: this.limit, page: this.page });
+    this.fetchAllProjects();
   },
   methods: {
-    async getProjects() {
-      const limit = this.limit;
-      const page = (this.page - 1) * this.limit || 0;
-
-      const data = Object.values(db.projects)
-        .filter((e) => e.isLock === false)
-        .filter((e) => e.isActive === true)
-        .sort(function (a, b) {
-          if (a.name > b.name) {
-            return 1;
-          }
-          if (a.name < b.name) {
-            return -1;
-          }
-          return 0;
-        })
-        .splice(page, limit)
-        .map((e) => {
-          return {
-            id: e.id,
-            name: e.name,
-            isActive: e.isActive,
-          };
-        });
-
-      this.projects = data;
-    },
-    async getTotalProjects() {
-      this.total = Object.values(db.projects).length;
-    },
+    ...mapActions(["fetchProjects", "fetchAllProjects"]),
+  },
+  computed: {
+    ...mapGetters(["getProjects", "getAllProjects"]),
   },
   watch: {
-    $route: ["getProjects", "getTotalProjects"],
+    $route: ["fetchProjects", "fetchAllProjects"],
   },
 };
 </script>
