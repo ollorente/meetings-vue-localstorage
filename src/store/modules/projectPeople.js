47653739bfffcb8b1039f54ea2c21326359dca05
Óscar Llorente <llorente_oscar@hotmail.com>
2021-03-30 00:18:05 -0500

    Ajustando lógica

--- src/store/modules/projectPeople.js
+++ src/store/modules/projectPeople.js
@@ -1,48 +1,83 @@
-import { db, dbName } from "@/main";
+import { db } from "@/main";
 
 const state = {
   projectsPeople: [],
 };
 
 const getters = {
-  getProjectsPeople: (state) => state.projectsPeople,
+  getAllProjectPeople: (state) => state.projectsPeople,
+  getProjectPeople: (state) => state.projectsPeople,
 };
 
 const actions = {
-  async addProjectsPeople({ commit }, data) {
+  async fetchProjectPeople({ commit }, data) {
     try {
-      const projectData = await db.projectPeople[data.id];
-      const collaborators = await data.collaborators;
+      const limit = data.limit ? data.limit : 20;
+      const page = (data.page - 1) * data.limit || 0;
 
-      if (!projectData) {
-        db.projectPeople[data.id] = {};
-      }
+      const people = Object.values(db.projectPeople[data.id])
+        .filter((e) => e.isLock === false)
+        .filter((e) => e.isActive === true)
+        .sort(function (a, b) {
+          if (a.name > b.name) {
+            return 1;
+          }
+          if (a.name < b.name) {
+            return -1;
+          }
+          return 0;
+        })
+        .splice(page, limit)
+        .map((e) => {
+          return {
+            id: e.id,
+            name: e.name,
+            email: e.email,
+            isActive: e.isActive,
+            isLock: e.isLock,
+          };
+        });
 
-      await collaborators.forEach(async (e) => {
-        const person = await db.people[e];
-
-        db.projectPeople[data.id][person.id] = {
-          id: person.id,
-          name: person.name,
-          email: person.email,
-          isActive: person.isActive,
-          isLock: person.isLock,
-        };
-      });
-
-      localStorage.setItem(dbName, JSON.stringify(db));
-
-      commit("SET_PROJECT_PERSON", true);
+      commit("SET_PROJECT_PEEOPLE", people);
     } catch (error) {
-      if (error) {
-        return;
-      }
+      if (error) return;
+    }
+  },
+
+  async fetchAllProjectPeople({ commit }, id) {
+    try {
+      const people = Object.values(db.projectPeople[id])
+        .filter((e) => e.isLock === false)
+        .filter((e) => e.isActive === true)
+        .sort(function (a, b) {
+          if (a.name > b.name) {
+            return 1;
+          }
+          if (a.name < b.name) {
+            return -1;
+          }
+          return 0;
+        })
+        .map((e) => {
+          return {
+            id: e.id,
+            name: e.name,
+            email: e.email,
+            isActive: e.isActive,
+            isLock: e.isLock,
+          };
+        });
+
+      commit("SET_ALL_PROJECT_PEEOPLE", people);
+    } catch (error) {
+      if (error) return;
     }
   },
 };
 
 const mutations = {
-  SET_PROJECT_PERSON: (state, project) => (state.projectsPeople = project),
+  SET_ALL_PROJECT_PEEOPLE: (state, project) => (state.projectsPeople = project),
+  SET_PROJECT_PEEOPLE: (state, project) => (state.projectsPeople = project),
 };
 
 export default {
