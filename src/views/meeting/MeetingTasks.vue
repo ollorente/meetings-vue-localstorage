b8e4481ea28516c385b382d05a729938355a95f7
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-02 21:25:22 -0500

    Conectando al store

--- src/views/meeting/MeetingTasks.vue
+++ src/views/meeting/MeetingTasks.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="tasks">
     <h1 style="margin: 0">
-      {{ total }}
-      {{ total === 1 ? "Tarea del proyecto" : "Tareas del proyecto" }}
+      {{ getAllMeetingTasks.length }}
+      {{
+        getAllMeetingTasks.length === 1
+          ? "Tarea de la reunión"
+          : "Tareas de la reunión"
+      }}
     </h1>
     <p>
       <router-link :to="{ name: 'NewMeetingTask' }" class="link"
@@ -15,7 +19,7 @@
         >Volver</router-link
       >
     </p>
-    <p v-for="(task, index) in tasks" :key="index" class="parrafo">
+    <p v-for="(task, index) in getMeetingTasks" :key="index" class="parrafo">
       <span class="parrafo__info">
         <span class="parrafo__info__number">{{ index + 1 }}</span>
         <span class="parrafo__info__name"
@@ -37,7 +41,7 @@
 </template>
 
 <script>
-import { db } from "@/main";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "Tasks",
@@ -46,50 +50,24 @@ export default {
     return {
       limit: parseInt(this.limit || 20),
       page: parseInt(this.page) > 0 ? parseInt(this.page || 1) : 1,
-      total: 0,
-      tasks: [],
     };
   },
   created() {
-    this.getTasks();
-    this.getTotalTasks();
+    this.fetchMeetingTasks({
+      id: this.$route.params.meeting,
+      limit: this.limit,
+      page: this.page,
+    });
+    this.fetchAllMeetingTasks(this.$route.params.meeting);
   },
   methods: {
-    async getTasks() {
-      const limit = this.limit;
-      const page = (this.page - 1) * this.limit || 0;
-
-      const data = Object.values(db.meetingTasks[this.$route.params.meeting])
-        .filter((e) => e.isLock === false)
-        .filter((e) => e.isActive === true)
-        .sort(function (a, b) {
-          if (a.name > b.name) {
-            return 1;
-          }
-          if (a.name < b.name) {
-            return -1;
-          }
-          return 0;
-        })
-        .splice(page, limit)
-        .map((e) => {
-          return {
-            id: e.id,
-            name: e.name,
-            isActive: e.isActive,
-          };
-        });
-
-      this.tasks = data;
-    },
-    async getTotalTasks() {
-      this.total = Object.values(db.meetingTasks[this.$route.params.meeting])
-        .filter((e) => e.isLock === false)
-        .filter((e) => e.isActive === true).length;
-    },
+    ...mapActions(["fetchMeetingTasks", "fetchAllMeetingTasks"]),
+  },
+  computed: {
+    ...mapGetters(["getMeetingTasks", "getAllMeetingTasks"]),
   },
   watch: {
-    $route: ["getTasks", "getTotalTasks"],
+    $route: ["fetchMeetingTasks", "fetchAllMeetingTasks"],
   },
 };
 </script>
