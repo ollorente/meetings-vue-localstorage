b2f9a25913eebf3be7807d315d196e3fb2a06e67
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-04 00:52:06 -0500

    Agregando componente

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main__navbar">
+    <router-link :to="{ name: 'Home' }" class="main__navbar__logo"
+      ><i :class="icon"></i> {{ titleApp }}</router-link
+    >
+    <div class="main__navbar__items">
+      <a href="#" class="main__navbar_items_link"
+        ><i class="fas fa-search"></i
+      ></a>
+      <a href="#" class="main__navbar_items_link"
+        ><i class="fas fa-ellipsis-v"></i
+      ></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheNavbar",
+  props: {
+    icon: String,
+    titleApp: String,
+  },
+};
+</script>
