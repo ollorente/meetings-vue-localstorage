e26d9fd6f69e3cc1445e4576d5566d3b922132f9
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-05 17:21:21 -0500

    Agregando diseño

--- src/views/meeting/MeetingPeople.vue
+++ src/views/meeting/MeetingPeople.vue
@@ -1,50 +1,79 @@
 <template>
-  <div class="people">
-    <h1 style="margin: 0">
-      {{ getAllMeetingPeople.length }}
-      {{
-        getAllMeetingPeople.length === 1
-          ? "Persona en la reunión"
-          : "Personas en la reunión"
-      }}
-    </h1>
-    <h2 style="margin: 0">{{ getMeeting.name }}</h2>
-    <p>
-      <router-link
-        :to="{ name: 'Meeting', params: { meeting: $route.params.meeting } }"
-        class="link"
-        >Volver</router-link
-      >
-    </p>
-    <p v-for="(person, index) in getMeetingPeople" :key="index" class="parrafo">
-      <span class="parrafo__info">
-        <span class="parrafo__info__number">{{ index + 1 }}</span>
-        <span class="parrafo__info__name"
-          ><router-link
-            :to="{ name: 'Person', params: { person: person.id } }"
-            :title="person.email"
-            class="link"
-            >{{ person.name }}</router-link
-          ></span
-        ></span
-      >
-      <span class="parrafo__status">{{
-        person.isActive ? "Activo" : "Inactivo"
-      }}</span>
-    </p>
-  </div>
+  <main class="main">
+    <TheSectionNavbar
+      :titleApp="titleApp"
+      :icon="icon"
+      :link="link"
+      :options="options"
+    />
+    <div class="main__body">
+      <div class="main__body__content">
+        <div class="main__body__section">
+          <div class="main__body__section__nav">
+            <h1 class="main__body__section__person__title">
+              {{ getMeeting.name }}
+            </h1>
+            <h3 class="main__body__section__person__subtitle"></h3>
+            <router-link
+              v-for="(person, index) in getMeetingPeople"
+              :key="index"
+              :to="{ name: 'Person', params: { person: person.id } }"
+              class="main__body__section__user"
+            >
+              <img
+                :src="
+                  person.photoURL
+                    ? person.photoURL
+                    : `https://res.cloudinary.com/dbszizqh4/image/upload/v1592198427/images_lvwix2.png`
+                "
+                :alt="person.name"
+                :title="person.email"
+                class="main__body__section__user__logo"
+              />
+              <div class="main__body__section__user__body">
+                <span class="main__body__section__user__title">{{
+                  person.name
+                }}</span>
+                <span class="main__body__section__user__content">{{
+                  person.email
+                }}</span>
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 
+import TheSectionNavbar from "@/components/TheSectionNavbar";
+
 export default {
   name: "MeetingPeople",
-  components: {},
+  components: {
+    TheSectionNavbar,
+  },
   data() {
     return {
       limit: parseInt(this.limit || 20),
       page: parseInt(this.page) > 0 ? parseInt(this.page || 1) : 1,
+      titleApp: "Usuarios de la reunión",
+      icon: "fas fa-arrow-left",
+      link: `/reunion/${this.$route.params.meeting}`,
+      options: [
+        {
+          menus: [
+            {
+              title: "Agregar usuario",
+              link: `/reunion/${this.$route.params.meeting}/usuario/nuevo`,
+              icon: "fas fa-user-plus",
+            },
+          ],
+        },
+      ],
     };
   },
   created() {
