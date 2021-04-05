8f64fdb2e550a5598023d7ca3f31a3ea474f4dea
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-05 17:20:32 -0500

    Agregando diseño

--- src/views/meeting/EditMeeting.vue
+++ src/views/meeting/EditMeeting.vue
@@ -1,76 +1,92 @@
 <template>
-  <div class="edit-meeting">
-    <h1 style="margin: 0">Editar reunión</h1>
-    <h2 style="margin: 0">{{ meeting.name }}</h2>
-    <!-- <h3 style="margin: 0">{{ project.name }}</h3> -->
-    <p>
-      <a href="#" @click="removeMeeting" class="link">Eliminar</a>
-      |
-      <router-link
-        :to="{ name: 'Meeting', params: { meeting: $route.params.meeting } }"
-        class="link"
-        >Volver</router-link
-      >
-    </p>
-    <form @submit.prevent="putMeeting">
-      <div>
-        <input
-          type="text"
-          v-model="meeting.name"
-          id="name"
-          placeholder="Nombre de proyecto"
-          autofocus
-          required
-        />
+  <main class="main">
+    <TheSectionNavbar
+      :titleApp="titleApp"
+      :icon="icon"
+      :link="link"
+      :options="options"
+    />
+    <div class="main__body">
+      <div class="main__body__content">
+        <div class="main__body__section">
+          <div class="main__body__section__nav">
+            <h1 class="main__body__section__nav--title">Editar reunión</h1>
+            <h2 class="main__body__section__nav--subtitle">
+              {{ meeting.name }}
+            </h2>
+            <form @submit.prevent="putMeeting">
+              <div>
+                <input
+                  type="text"
+                  v-model="meeting.name"
+                  id="name"
+                  placeholder="Nombre de proyecto"
+                  autofocus
+                  required
+                />
+              </div>
+              <div>
+                <textarea
+                  v-model="meeting.description"
+                  id="description"
+                  rows="10"
+                  placeholder="Agregue una descripción"
+                ></textarea>
+              </div>
+              <div>
+                <input
+                  type="datetime-local"
+                  v-model="meeting.dateInt"
+                  id="dateInt"
+                />
+              </div>
+              <div>
+                <input
+                  type="datetime-local"
+                  v-model="meeting.dateEnd"
+                  id="dateEnd"
+                />
+              </div>
+              <div>
+                <select
+                  multiple
+                  v-model="meeting.collaborators"
+                  id="collaborators"
+                >
+                  <option
+                    v-for="person in getAllMeetingPeople"
+                    :key="person.id"
+                    :value="person.id"
+                  >
+                    {{ person.name }} - {{ person.email }}
+                  </option>
+                </select>
+              </div>
+              <div>
+                <label
+                  ><input type="checkbox" v-model="meeting.isActive" />
+                  Estatus</label
+                >
+              </div>
+              <button type="submit" class="btn-secondary">Actualizar</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div>
-        <textarea v-model="meeting.description" id="description" rows="10">
-          Agregue una descripción</textarea
-        >
-      </div>
-      <div>
-        <input type="datetime-local" v-model="meeting.dateInt" id="dateInt" />
-      </div>
-      <div>
-        <input type="datetime-local" v-model="meeting.dateEnd" id="dateEnd" />
-      </div>
-      <div>
-        <select multiple v-model="meeting.collaborators" id="collaborators">
-          <option
-            v-for="person in getAllMeetingPeople"
-            :key="person.id"
-            :value="person.id"
-          >
-            {{ person.name }} - {{ person.email }}
-          </option>
-        </select>
-      </div>
-      <div>
-        <label
-          ><input type="checkbox" v-model="meeting.isActive" /> Estatus</label
-        >
-      </div>
-      <button type="submit">Actualizar</button>
-    </form>
-    <pre class="container" hidden style="text-align: left">{{ $data }}</pre>
-    <pre class="container" hidden style="text-align: left">{{
-      getMeeting
-    }}</pre>
-    <pre class="container" hidden style="text-align: left">{{
-      getAllMeetingPeople
-    }}</pre>
-    <div id="alert" v-if="alert.error">
-      {{ alert.msg }}
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 
+import TheSectionNavbar from "@/components/TheSectionNavbar";
+
 export default {
   name: "EditMeeting",
-  components: {},
+  components: {
+    TheSectionNavbar,
+  },
   data() {
     return {
       meeting: "",
@@ -79,6 +95,10 @@ export default {
         error: false,
         msg: null,
       },
+      titleApp: "Editar reunión",
+      icon: "fas fa-arrow-left",
+      link: `/reunion/${this.$route.params.meeting}`,
+      options: [],
     };
   },
   mounted() {
