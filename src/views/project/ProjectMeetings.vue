f2c0a97bf6b6d915ef6b1b4b8d2ec82c19a2960b
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-05 17:24:40 -0500

    Agregando diseño

--- src/views/project/ProjectMeetings.vue
+++ src/views/project/ProjectMeetings.vue
@@ -1,71 +1,82 @@
 <template>
-  <div class="projects">
-    <h1 style="margin: 0">
-      {{ getAllProjectMeetings.length }}
-      {{
-        getAllProjectMeetings.length === 1
-          ? "Reunión del proyecto"
-          : "Reuniones del proyecto"
-      }}
-    </h1>
-    <p>
-      <router-link
-        :to="{
-          name: 'NewProjectMeeting',
-          params: { project: $route.params.project },
-        }"
-        class="link"
-        >Agregar</router-link
-      >
-      |
-      <router-link
-        :to="{ name: 'Project', params: { project: $route.params.project } }"
-        class="link"
-        >Volver</router-link
-      >
-    </p>
-    <p
-      v-for="(meeting, index) in getProjectMeetings"
-      :key="index"
-      class="parrafo"
-    >
-      <span class="parrafo__info">
-        <span class="parrafo__info__number">{{ index + 1 }}</span>
-        <span class="parrafo__info__name"
-          ><router-link
-            :to="{ name: 'Meeting', params: { meeting: meeting.id } }"
-            class="link"
-            >{{ meeting.name }}</router-link
-          ></span
-        ></span
-      >
-      <span class="parrafo__status" style="display: flex">
-        <span style="padding: 0 0.5rem; font-size: 0.8rem; font-weight: 600">{{
-          new Date(meeting.dateInt).getMonth() + 1
-        }}</span>
-        <span style="padding: 0 0.5rem; font-size: 2rem; font-weight: 600">{{
-          new Date(meeting.dateInt).toString().split(" ")[2]
-        }}</span>
-        <span
-          >{{ new Date(meeting.dateInt).toString().split(" ")[4] }}<br />{{
-            new Date(meeting.dateEnd).toString().split(" ")[4]
-          }}</span
-        >
-      </span>
-    </p>
-  </div>
+  <main class="main">
+    <TheSectionNavbar
+      :titleApp="titleApp"
+      :icon="icon"
+      :link="link"
+      :options="options"
+    />
+    <div class="main__body">
+      <div class="main__body__content">
+        <div class="main__body__section">
+          <div class="main__body__section__nav">
+            <h1 class="main__body__section__person__title">
+              {{ getProject.name }}
+            </h1>
+            <h3 class="main__body__section__person__subtitle"></h3>
+            <div
+              v-for="(meeting, index) in getProjectMeetings"
+              :key="index"
+              class="main__body__section__card"
+            >
+              <div class="main__body__section__card__date">
+                <span class="main__body__section__card__month">{{
+                  new Date(meeting.dateInt).getMonth() + 1
+                }}</span>
+                <span class="main__body__section__card__day">{{
+                  new Date(meeting.dateInt).toString().split(" ")[2]
+                }}</span>
+              </div>
+              <router-link
+                :to="{ name: 'Meeting', params: { meeting: meeting.id } }"
+                class="main__body__section__card__body"
+              >
+                <span class="main__body__section__link__text__content">
+                  <span class="text-title">{{ meeting.name }}</span>
+                  <span class="text-content"
+                    >{{ new Date(meeting.dateInt).toString().split(" ")[4] }} -
+                    {{
+                      new Date(meeting.dateEnd).toString().split(" ")[4]
+                    }}</span
+                  >
+                </span>
+                <span class="main__body__section__link__icon">
+                  <i
+                    :class="meeting.isActive ? 'fas' : 'far'"
+                    class="fa-circle"
+                  ></i>
+                  <i
+                    :class="meeting.isLock ? 'fas' : 'far'"
+                    class="fa-circle"
+                  ></i>
+                </span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 
+import TheSectionNavbar from "@/components/TheSectionNavbar";
+
 export default {
   name: "ProjectMeetings",
-  components: {},
+  components: {
+    TheSectionNavbar,
+  },
   data() {
     return {
       limit: parseInt(this.limit || 20),
       page: parseInt(this.page) > 0 ? parseInt(this.page || 1) : 1,
+      titleApp: "Reuniones del proyecto",
+      icon: "fas fa-arrow-left",
+      link: `/proyecto/${this.$route.params.project}`,
+      options: [],
     };
   },
   created() {
@@ -74,16 +85,16 @@ export default {
       limit: this.limit,
       page: this.page,
     });
-    this.fetchAllProjectMeetings();
+    this.fetchProject(this.$route.params.project);
   },
   methods: {
-    ...mapActions(["fetchProjectMeetings", "fetchAllProjectMeetings"]),
+    ...mapActions(["fetchProjectMeetings", "fetchProject"]),
   },
   computed: {
-    ...mapGetters(["getProjectMeetings", "getAllProjectMeetings"]),
+    ...mapGetters(["getProjectMeetings", "getProject"]),
   },
   watch: {
-    $route: ["fetchProjectMeetings", "fetchAllProjectMeetings"],
+    $route: ["fetchProjectMeetings", "fetchProject"],
   },
 };
 </script>
