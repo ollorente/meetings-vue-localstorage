23de357c9b3b3156b11c61d34e06b23816f8d462
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-05 00:20:54 -0500

    Ajustando modal

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="main__navbar">
-    <router-link :to="{ name: 'Home' }" class="main__navbar__logo"
-      ><i :class="icon"></i> {{ titleApp }}</router-link
-    >
+    <div class="main__navbar__logo"><i :class="icon"></i> {{ titleApp }}</div>
     <div class="main__navbar__items">
       <a href="#" class="main__navbar_items_link"
         ><i class="fas fa-search"></i
       ></a>
-      <a href="#" class="main__navbar_items_link"
+      <span class="main__navbar_items_link" v-if="options" @click="isVisible"
         ><i class="fas fa-ellipsis-v"></i
-      ></a>
+      ></span>
+    </div>
+    <div class="options" v-if="show">
+      <div class="options__card">
+        <ul class="options__card__items">
+          <li class="options__card__icon">
+            <span>Opciones</span>
+            <i class="fas fa-chevron-up" @click="isVisible"></i>
+          </li>
+          <li
+            v-for="(menu, index) in options[0].menus"
+            :key="index"
+            class="options__card__item"
+          >
+            <a :href="menu.link" class="options__card__link"
+              ><i :class="menu.icon"></i> {{ menu.title }}</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +37,18 @@ export default {
   props: {
     icon: String,
     titleApp: String,
+    link: String,
+    options: Array,
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    async isVisible() {
+      this.show = !this.show;
+    },
   },
 };
 </script>
