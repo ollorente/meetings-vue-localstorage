fb24c29f015750bc6270b14b1fb822e7dfe21cb6
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-05 00:21:37 -0500

    Agregando navbar

---
+++ src/components/TheSectionNavbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main__navbar">
+    <a :href="link" class="main__navbar__logo"
+      ><i :class="icon"></i> {{ titleApp }}</a
+    >
+    <div class="main__navbar__items" v-if="options.length > 0">
+      <span class="main__navbar_items_link" @click="isVisible"
+        ><i class="fas fa-ellipsis-v"></i
+      ></span>
+    </div>
+    <div class="options" v-if="show">
+      <div class="options__card">
+        <ul class="options__card__items">
+          <li class="options__card__icon">
+            <span>Opciones</span>
+            <i class="fas fa-chevron-up" @click="isVisible"></i>
+          </li>
+          <li
+            v-for="(menu, index) in options[0].menus"
+            :key="index"
+            class="options__card__item"
+          >
+            <a :href="menu.link" class="options__card__link"
+              ><i :class="menu.icon"></i> {{ menu.title }}</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheSectionNavbar",
+  props: {
+    icon: String,
+    titleApp: String,
+    link: String,
+    options: Array,
+  },
+  data() {
+    return {
+      show: false,
+      isParams: this.$props.params ? `params: ${this.$props.params}` : "",
+    };
+  },
+  methods: {
+    async isVisible() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
