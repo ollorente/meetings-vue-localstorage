6b3cf765be655fd5e0c0ba2c270acf6c72636663
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-05 00:22:29 -0500

    Agregando diseño

--- src/views/person/EditPerson.vue
+++ src/views/person/EditPerson.vue
@@ -1,78 +1,101 @@
 <template>
-  <div class="edit-person">
-    <h1 style="margin: 0">Editar "{{ person.email }}"</h1>
-    <p>
-      <a href="#" @click="removePerson" class="link">Eliminar</a>
-      |
-      <router-link
-        :to="{ name: 'Person', params: { person: this.$route.params.person } }"
-        class="link"
-        >Volver</router-link
-      >
-    </p>
-    <form @submit.prevent="putPerson">
-      <div>
-        <input
-          type="text"
-          v-model="person.name"
-          id="name"
-          placeholder="Nombre de usuario"
-          autofocus
-          required
-        />
+  <main class="main">
+    <Alert :msg="alert.msg" v-if="alert.error" />
+    <TheSectionNavbar
+      :titleApp="titleApp"
+      :icon="icon"
+      :link="link"
+      :options="options"
+    />
+    <div class="main__body">
+      <div class="main__body__content">
+        <div class="main__body__section">
+          <div class="main__body__section__nav">
+            <h1 class="main__body__section__nav--title">Editar usuario</h1>
+            <form @submit.prevent="putPerson">
+              <div>
+                <input
+                  type="text"
+                  v-model="person.name"
+                  id="name"
+                  placeholder="Nombre de usuario"
+                  autofocus
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  v-model="person.email"
+                  id="email"
+                  placeholder="email@email.com"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  v-model="person.role"
+                  id="role"
+                  placeholder="Cargo"
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  v-model="person.photoURL"
+                  id="photoURL"
+                  placeholder="Imagen de avatar"
+                />
+              </div>
+              <div hidden>
+                <label for="isActive" class="form-label"
+                  ><input type="checkbox" v-model="person.isActive" />
+                  Estatus</label
+                >
+              </div>
+              <div>
+                <label for="isActive" class="form-label" @click="isActive"
+                  ><i
+                    :class="person.isActive ? 'fas' : 'far'"
+                    class="fa-circle"
+                  ></i>
+                  Activo</label
+                >
+              </div>
+              <button type="submit" class="btn-secondary">Actualizar</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div>
-        <input
-          type="email"
-          v-model="person.email"
-          id="email"
-          placeholder="email@email.com"
-          required
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          v-model="person.role"
-          id="role"
-          placeholder="Cargo"
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          v-model="person.photoURL"
-          id="photoURL"
-          placeholder="Imagen de avatar"
-        />
-      </div>
-      <div>
-        <label
-          ><input type="checkbox" v-model="person.isActive" /> Estatus</label
-        >
-      </div>
-      <button type="submit">Actualizar</button>
-    </form>
-    <div id="alert" v-if="alert.error">
-      {{ alert.msg }}
     </div>
-    <pre class="container" hidden style="text-align: left">{{ $data }}</pre>
-  </div>
+    <pre class="container" hidden>{{ $data }}</pre>
+  </main>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 
+import Alert from "@/components/Alert";
+import TheSectionNavbar from "@/components/TheSectionNavbar";
+
 export default {
   name: "EditPerson",
-  components: {},
+  components: {
+    Alert,
+    TheSectionNavbar,
+  },
   data() {
     return {
       person: "",
+      titleApp: "Editar usuario",
+      icon: "fas fa-arrow-left",
+      link: `/usuario/${this.$route.params.person}`,
       alert: {
-        error: true,
+        error: false,
         msg: null,
       },
+      options: [],
     };
   },
   mounted() {
@@ -94,17 +117,31 @@ export default {
   methods: {
     ...mapActions(["deletePerson", "fetchPerson", "updatePerson"]),
     async putPerson() {
-      await this.updatePerson(this.person);
+      if (this.person.name.trim() === "" || this.person.email.trim() === "") {
+        this.alert.error = true;
+        this.alert.msg = `El Correo y/o Email no pueden ser vacios.`;
 
-      this.person.name = "";
-      this.person.email = "";
-      this.person.role = "";
-      this.person.photoURL = "";
+        setTimeout(() => {
+          this.alert.error = false;
+        }, 4000);
 
-      await this.$router.replace({
-        name: "Person",
-        params: { person: this.$route.params.person },
-      });
+        return;
+      } else {
+        await this.updatePerson(this.person);
+
+        this.person.name = "";
+        this.person.email = "";
+        this.person.role = "";
+        this.person.photoURL = "";
+
+        await this.$router.replace({
+          name: "Person",
+          params: { person: this.$route.params.person },
+        });
+      }
+    },
+    async isActive() {
+      this.person.isActive = !this.person.isActive;
     },
     async removePerson() {
       if (window.confirm(`Está a punto de borrar un elemento`)) {
