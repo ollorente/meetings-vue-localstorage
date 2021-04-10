3f6d5ba7d211bddcc8d640b80582690d9fd28def
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-10 17:52:14 -0500

    Agregando elementos y lógica

--- src/views/person/EditPerson.vue
+++ src/views/person/EditPerson.vue
@@ -4,7 +4,54 @@
     <main>
       <transition name="fade">
         <section class="section">
-          <h1 class="title">{{ getPerson.name }}</h1>
+          <Alert :msg="alert.msg" v-if="alert.error" />
+          <h1 class="title">{{ person.name }}</h1>
+          <form @submit.prevent="putPerson">
+            <div>
+              <input
+                type="text"
+                v-model="person.name"
+                id="name"
+                placeholder="Nombre de usuario"
+                autofocus
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="email"
+                v-model="person.email"
+                id="email"
+                placeholder="email@email.com"
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                v-model="person.photoURL"
+                id="role"
+                placeholder="Imagen del usuario"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                v-model="person.phone"
+                id="role"
+                placeholder="Teléfono"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                v-model="person.role"
+                id="role"
+                placeholder="Cargo"
+              />
+            </div>
+            <button type="submit" class="btn-secondary">Editar</button>
+          </form>
         </section>
       </transition>
     </main>
@@ -12,7 +59,8 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions } from 'vuex'
+import { db } from '@/main'
 
 import TheNavbar from '@/components/TheNavbar'
 
@@ -34,20 +82,77 @@ export default {
         {
           menus: []
         }
-      ]
+      ],
+      alert: {
+        error: false,
+        msg: null
+      },
+      person: {
+        id: '',
+        name: '',
+        email: '',
+        photoURL: '',
+        phone: '',
+        role: '',
+        isLock: '',
+        isActive: '',
+        createdAt: '',
+        updatedAt: ''
+      }
     }
   },
   created () {
-    this.fetchPerson(this.$route.params.person)
+    this.getPerson()
   },
   methods: {
-    ...mapActions(['fetchPerson'])
-  },
-  computed: {
-    ...mapGetters(['getPerson'])
+    ...mapActions(['getPerson', 'updatePerson']),
+    async getPerson () {
+      const data = await db.people[this.$route.params.person]
+
+      this.person = {
+        id: await data.id,
+        name: await data.name,
+        email: await data.email,
+        photoURL: await data.photoURL,
+        phone: await data.phone,
+        role: await data.role,
+        isLock: await data.isLock,
+        isActive: await data.isActive,
+        createdAt: await data.createdAt,
+        updatedAt: await data.updatedAt
+      }
+    },
+    async putPerson () {
+      try {
+        if (this.person.name.trim() === '' || this.person.email.trim() === '') {
+          this.alert.error = true
+          this.alert.msg = `Los campos no pueden estar vacíos.`
+
+          setTimeout(() => {
+            this.alert.error = false
+          }, 4000)
+        } else {
+          await this.updatePerson(this.person)
+
+          this.person.name = ''
+          this.person.email = ''
+          this.person.photoURL = ''
+          this.person.phone = ''
+          this.person.role = ''
+
+          await this.$router.replace({
+            name: 'Person',
+            params: { person: this.$route.params.person }
+          })
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-useless-return
+        if (error) return
+      }
+    }
   },
   watch: {
-    $route: ['fetchPerson']
+    $route: ['getPerson']
   }
 }
 </script>
