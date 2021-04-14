704b7a5421bad0111b90bc2b4e53ab7cb7edd1c8
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-13 21:34:19 -0500

    Agregando búsqueda

--- src/views/meeting/SearchMeetingTasks.vue
+++ src/views/meeting/SearchMeetingTasks.vue
@@ -5,7 +5,20 @@
       <transition name="fade">
         <section class="section">
           <h1 class="title">{{ getMeeting.name }}</h1>
+          <div class="navbar__search">
+            <form @submit.prevent="search">
+              <input
+                type="text"
+                class="navbar__search--input mb-3"
+                placeholder="Buscar..."
+                v-model="q"
+                @keyup="search"
+                autofocus
+              />
+            </form>
+          </div>
           <Task v-for='task in tasks' :key='task.id' :task='task' />
+          <div class="my-3" v-if="show">No hay resultados</div>
         </section>
       </transition>
     </main>
@@ -14,6 +27,7 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import { db } from '@/main'
 
 import TheNavbar from '@/components/TheNavbar'
 import Task from '@/components/gadgets/Task'
@@ -29,12 +43,12 @@ export default {
       path: {
         title: 'Actividades encuentro',
         link: {
-          name: 'Meeting',
+          name: 'MeetingTasks',
           params: { meeting: this.$route.params.meeting }
         },
         icon: 'fas fa-arrow-left',
         status: false,
-        search: true
+        search: false
       },
       options: [
         {
@@ -50,40 +64,56 @@ export default {
           ]
         }
       ],
+      show: true,
       tasks: [],
       limit: 10,
-      page: 0
+      page: 0,
+      q: ''
     }
   },
   created () {
     this.fetchMeeting(this.$route.params.meeting)
   },
   methods: {
-    ...mapActions(['fetchMeetingTasks', 'fetchMeeting']),
-    async infiniteHandler ($state) {
-      this.page++
+    ...mapActions(['fetchMeeting']),
+    async search () {
+      const tasks = Object.values(db.meetingTasks[this.$route.params.meeting])
+      const texto = this.q.toLowerCase()
 
-      this.fetchMeetingTasks({
-        id: this.$route.params.meeting,
-        limit: this.limit,
-        page: this.page
-      })
+      this.tasks = []
 
-      let tasks = await this.getMeetingTasks
+      for (let task of tasks) {
+        let data = task.name.toLowerCase()
 
-      if (tasks.length) {
-        this.tasks = this.tasks.concat(tasks)
-        $state.loaded()
-      } else {
-        $state.complete()
+        if (data.indexOf(texto) !== -1) {
+          this.tasks = this.tasks
+            .concat(task)
+            .filter((e) => e.isActive === true)
+            .sort(function (a, b) {
+              if (a.name > b.name) {
+                return 1
+              }
+              if (a.name < b.name) {
+                return -1
+              }
+              return 0
+            })
+            .splice(this.page, this.limit)
+        }
+
+        if (this.tasks.length === 0) {
+          this.show = true
+        } else {
+          this.show = false
+        }
       }
     }
   },
   computed: {
-    ...mapGetters(['getMeetingTasks', 'getMeeting'])
+    ...mapGetters(['getMeeting'])
   },
   watch: {
-    $route: ['fetchMeetingTasks', 'fetchMeeting']
+    $route: ['fetchMeeting']
   }
 }
 </script>
