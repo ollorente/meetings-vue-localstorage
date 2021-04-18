245a048a577dad5bdaee39b08dfad7755ebb1669
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-17 19:24:45 -0500

    Protegiendo rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from '@/store'
 
 Vue.use(Router)
 
@@ -35,7 +36,8 @@ const routes = [
     name: 'Projects',
     component: () => import('../views/project/Projects.vue'),
     meta: {
-      title: `Proyectos ${BASE_URL_TITLE}`
+      title: `Proyectos ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -43,7 +45,8 @@ const routes = [
     name: 'SearchProjects',
     component: () => import('../views/project/SearchProjects.vue'),
     meta: {
-      title: `Buscar proyecto ${BASE_URL_TITLE}`
+      title: `Buscar proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -51,7 +54,8 @@ const routes = [
     name: 'NewProject',
     component: () => import('../views/project/NewProject.vue'),
     meta: {
-      title: `Agreagr proyecto ${BASE_URL_TITLE}`
+      title: `Agreagr proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -59,7 +63,8 @@ const routes = [
     name: 'Project',
     component: () => import('../views/project/Project.vue'),
     meta: {
-      title: `Proyecto ${BASE_URL_TITLE}`
+      title: `Proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -67,7 +72,8 @@ const routes = [
     name: 'EditProject',
     component: () => import('../views/project/EditProject.vue'),
     meta: {
-      title: `Editar proyecto ${BASE_URL_TITLE}`
+      title: `Editar proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -75,7 +81,8 @@ const routes = [
     name: 'ProjectMeetings',
     component: () => import('../views/project/ProjectMeetings.vue'),
     meta: {
-      title: `Encuentros proyecto ${BASE_URL_TITLE}`
+      title: `Encuentros proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -83,7 +90,8 @@ const routes = [
     name: 'SearchProjectMeetings',
     component: () => import('../views/project/SearchProjectMeetings.vue'),
     meta: {
-      title: `Buscar encuentro proyecto ${BASE_URL_TITLE}`
+      title: `Buscar encuentro proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -91,7 +99,8 @@ const routes = [
     name: 'NewProjectMeeting',
     component: () => import('../views/project/NewProjectMeeting.vue'),
     meta: {
-      title: `Agregar encuentro ${BASE_URL_TITLE}`
+      title: `Agregar encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -99,7 +108,8 @@ const routes = [
     name: 'ProjectTasks',
     component: () => import('../views/project/ProjectTasks.vue'),
     meta: {
-      title: `Actividades proyecto ${BASE_URL_TITLE}`
+      title: `Actividades proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -107,7 +117,8 @@ const routes = [
     name: 'SearchProjectTasks',
     component: () => import('../views/project/SearchProjectTasks.vue'),
     meta: {
-      title: `Buscar actividad proyecto ${BASE_URL_TITLE}`
+      title: `Buscar actividad proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -115,7 +126,8 @@ const routes = [
     name: 'ProjectPeople',
     component: () => import('../views/project/ProjectPeople.vue'),
     meta: {
-      title: `Usuarios proyecto ${BASE_URL_TITLE}`
+      title: `Usuarios proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -123,7 +135,8 @@ const routes = [
     name: 'SearchProjectPeople',
     component: () => import('../views/project/SearchProjectPeople.vue'),
     meta: {
-      title: `Buscar usuario proyecto ${BASE_URL_TITLE}`
+      title: `Buscar usuario proyecto ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -131,7 +144,8 @@ const routes = [
     name: 'Meetings',
     component: () => import('../views/meeting/Meetings.vue'),
     meta: {
-      title: `Encuentros ${BASE_URL_TITLE}`
+      title: `Encuentros ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -139,7 +153,8 @@ const routes = [
     name: 'SearchMeetings',
     component: () => import('../views/meeting/SearchMeetings.vue'),
     meta: {
-      title: `Buscar encuentro ${BASE_URL_TITLE}`
+      title: `Buscar encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -147,7 +162,8 @@ const routes = [
     name: 'Meeting',
     component: () => import('../views/meeting/Meeting.vue'),
     meta: {
-      title: `Encuentro ${BASE_URL_TITLE}`
+      title: `Encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -155,7 +171,8 @@ const routes = [
     name: 'EditMeeting',
     component: () => import('../views/meeting/EditMeeting.vue'),
     meta: {
-      title: `Editar encuentro ${BASE_URL_TITLE}`
+      title: `Editar encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -163,7 +180,8 @@ const routes = [
     name: 'MeetingTasks',
     component: () => import('../views/meeting/MeetingTasks.vue'),
     meta: {
-      title: `Actividades encuentro ${BASE_URL_TITLE}`
+      title: `Actividades encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -171,7 +189,8 @@ const routes = [
     name: 'SearchMeetingTasks',
     component: () => import('../views/meeting/SearchMeetingTasks.vue'),
     meta: {
-      title: `Buscar actividad encuentro ${BASE_URL_TITLE}`
+      title: `Buscar actividad encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -179,7 +198,8 @@ const routes = [
     name: 'NewMeetingTask',
     component: () => import('../views/meeting/NewMeetingTask.vue'),
     meta: {
-      title: `Agregar actividad ${BASE_URL_TITLE}`
+      title: `Agregar actividad ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -187,7 +207,8 @@ const routes = [
     name: 'MeetingPeople',
     component: () => import('../views/meeting/MeetingPeople.vue'),
     meta: {
-      title: `Usuarios encuentro ${BASE_URL_TITLE}`
+      title: `Usuarios encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -195,7 +216,8 @@ const routes = [
     name: 'SearchMeetingPeople',
     component: () => import('../views/meeting/SearchMeetingPeople.vue'),
     meta: {
-      title: `Buscar usuario encuentro ${BASE_URL_TITLE}`
+      title: `Buscar usuario encuentro ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -203,7 +225,8 @@ const routes = [
     name: 'Tasks',
     component: () => import('../views/task/Tasks.vue'),
     meta: {
-      title: `Actividades ${BASE_URL_TITLE}`
+      title: `Actividades ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -211,7 +234,8 @@ const routes = [
     name: 'SearchTasks',
     component: () => import('../views/task/SearchTasks.vue'),
     meta: {
-      title: `Buscar actividad ${BASE_URL_TITLE}`
+      title: `Buscar actividad ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -219,7 +243,8 @@ const routes = [
     name: 'Task',
     component: () => import('../views/task/Task.vue'),
     meta: {
-      title: `Actividad ${BASE_URL_TITLE}`
+      title: `Actividad ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -227,7 +252,8 @@ const routes = [
     name: 'EditTask',
     component: () => import('../views/task/EditTask.vue'),
     meta: {
-      title: `Editar actividad ${BASE_URL_TITLE}`
+      title: `Editar actividad ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -235,7 +261,8 @@ const routes = [
     name: 'TaskPeople',
     component: () => import('../views/task/TaskPeople.vue'),
     meta: {
-      title: `Usuarios actividad ${BASE_URL_TITLE}`
+      title: `Usuarios actividad ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -243,7 +270,8 @@ const routes = [
     name: 'SearchTaskPeople',
     component: () => import('../views/task/SearchTaskPeople.vue'),
     meta: {
-      title: `Buscar usuario actividad ${BASE_URL_TITLE}`
+      title: `Buscar usuario actividad ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -251,7 +279,8 @@ const routes = [
     name: 'People',
     component: () => import('../views/person/People.vue'),
     meta: {
-      title: `Usuarios ${BASE_URL_TITLE}`
+      title: `Usuarios ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -259,7 +288,8 @@ const routes = [
     name: 'SearchPeople',
     component: () => import('../views/person/SearchPeople.vue'),
     meta: {
-      title: `Buscar usuario ${BASE_URL_TITLE}`
+      title: `Buscar usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -267,7 +297,8 @@ const routes = [
     name: 'NewPerson',
     component: () => import('../views/person/NewPerson.vue'),
     meta: {
-      title: `Agregar usuario ${BASE_URL_TITLE}`
+      title: `Agregar usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -275,7 +306,8 @@ const routes = [
     name: 'Person',
     component: () => import('../views/person/Person.vue'),
     meta: {
-      title: `Usuario ${BASE_URL_TITLE}`
+      title: `Usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -283,7 +315,8 @@ const routes = [
     name: 'EditPerson',
     component: () => import('../views/person/EditPerson.vue'),
     meta: {
-      title: `Editar usuario ${BASE_URL_TITLE}`
+      title: `Editar usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -291,7 +324,8 @@ const routes = [
     name: 'PersonProjects',
     component: () => import('../views/person/PersonProjects.vue'),
     meta: {
-      title: `Proyectos usuario ${BASE_URL_TITLE}`
+      title: `Proyectos usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -299,7 +333,8 @@ const routes = [
     name: 'SearchPersonProjects',
     component: () => import('../views/person/SearchPersonProjects.vue'),
     meta: {
-      title: `Buscar proyecto usuario ${BASE_URL_TITLE}`
+      title: `Buscar proyecto usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -307,7 +342,8 @@ const routes = [
     name: 'PersonMeetings',
     component: () => import('../views/person/PersonMeetings.vue'),
     meta: {
-      title: `Encuentros usuario ${BASE_URL_TITLE}`
+      title: `Encuentros usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -315,7 +351,8 @@ const routes = [
     name: 'SearchPersonMeetings',
     component: () => import('../views/person/SearchPersonMeetings.vue'),
     meta: {
-      title: `Buscar encuentro usuario ${BASE_URL_TITLE}`
+      title: `Buscar encuentro usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -323,7 +360,8 @@ const routes = [
     name: 'PersonTasks',
     component: () => import('../views/person/PersonTasks.vue'),
     meta: {
-      title: `Actividades usuario ${BASE_URL_TITLE}`
+      title: `Actividades usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
@@ -331,11 +369,21 @@ const routes = [
     name: 'SearchPersonTasks',
     component: () => import('../views/person/SearchPersonTasks.vue'),
     meta: {
-      title: `Buscar actividad usuario ${BASE_URL_TITLE}`
+      title: `Buscar actividad usuario ${BASE_URL_TITLE}`,
+      privateRoute: true
     }
   },
   {
-    path: '/*',
+    path: '/perfil',
+    name: 'Profile',
+    component: () => import('../views/profile/Profile.vue'),
+    meta: {
+      title: `Perfil ${BASE_URL_TITLE}`,
+      privateRoute: true
+    }
+  },
+  {
+    path: '*',
     name: 'Error',
     component: () => import('../views/Error.vue'),
     meta: {
@@ -353,6 +401,14 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
   next()
+
+  const privateRuote = to.matched.some(item => item.meta.privateRuote)
+
+  if (privateRuote && store.state.token === null) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
 })
 
 export default router
