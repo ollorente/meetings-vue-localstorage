982febb8c17b8968ec20a712092d964319714343
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-22 17:25:20 -0500

    Cambiando nombre en el buscador

--- src/views/meeting/SearchMeetings.vue
+++ src/views/meeting/SearchMeetings.vue
@@ -10,7 +10,7 @@
               <input
                 type="text"
                 class="navbar__search--input mb-3"
-                placeholder="Buscar..."
+                placeholder="Buscar encuentro..."
                 v-model="q"
                 @keyup="search"
                 autofocus
