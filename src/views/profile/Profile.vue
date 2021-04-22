4d5afa276e4c918c5cda3f9f280582a0a353dbb0
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-22 17:49:58 -0500

    Agregando sitio para perfil

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,9 +1,40 @@
 <template>
-  <h1>Perfil</h1>
+  <div class="content">
+    <MainNavbar :path="path" :options="options" />
+    <main>
+      <transition name="fade">
+        <section class="section">
+          <h1>Perfil</h1>
+          <p></p>
+        </section>
+      </transition>
+    </main>
+  </div>
 </template>
 
 <script>
+import MainNavbar from '@/components/MainNavbar'
+
 export default {
-  name: 'Profile'
+  name: 'Profile',
+  components: {
+    MainNavbar
+  },
+  data () {
+    return {
+      path: {
+        title: 'Perfil',
+        link: { name: 'Tasks' },
+        icon: 'fas fa-arrow-left',
+        status: false,
+        search: false
+      },
+      options: [
+        {
+          menus: []
+        }
+      ]
+    }
+  }
 }
 </script>
