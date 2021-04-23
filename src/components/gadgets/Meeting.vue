4666c7953078e3d6220e8ac36a2278ec4cb3468a
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-23 18:06:41 -0500

    Formateando fechas

--- src/components/gadgets/Meeting.vue
+++ src/components/gadgets/Meeting.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="main__section__card">
     <div class="main__section__card__date">
-      <span class="main__section__card__month">{{
-        new Date(meeting.dateInt).toDateString().substr(4, 3)
-      }}</span>
-      <span class="main__section__card__day">{{
-        new Date(meeting.dateInt).toDateString().substr(8, 2)
-      }}</span>
+      <span class="main__section__card__month">
+        {{ new Date(meeting.dateInt).toDateString().substr(4, 3) }}
+      </span>
+      <span class="main__section__card__day">
+        {{ new Date(meeting.dateInt).toDateString().substr(8, 2) }}
+      </span>
     </div>
     <router-link
       :to="{ name: 'Meeting', params: { meeting: meeting._id } }"
@@ -15,10 +15,11 @@
     >
       <span class="main__section__link__text__content">
         <span class="text-title">{{ meeting.name }}</span>
-        <span class="text-content"
-          >{{ meeting.dateInt.substr(11, 5) }} -
-          {{ meeting.dateEnd.substr(11, 5) }}</span
-        >
+        <span class="text-content">
+          {{ new Date(meeting.dateInt).getUTCHours() }}:{{ new Date(meeting.dateInt).getUTCMinutes() }}
+          -
+          {{ new Date(meeting.dateEnd).getUTCHours() }}:{{ new Date(meeting.dateEnd).getUTCMinutes() }}
+        </span>
       </span>
     </router-link>
     <router-link
