6c47da48862703e8b2267400989f5c3752c0cd84
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-23 18:13:45 -0500

    Formateando fechas y conectando al store

--- src/views/task/Task.vue
+++ src/views/task/Task.vue
@@ -5,34 +5,40 @@
       <transition name="fade">
         <section class="section">
           <div class="main__section__person">
-            <h1 class="title">{{ task.name }}</h1>
-            <div v-html="task.description"></div>
+            <h1 class="title">{{ getTask.name }}</h1>
+            <div v-html="getTask.description"></div>
             <p class="main__section__person__block">
               <span class="main__section__person__block__content">
-                <b>{{ task.collaborators.length }}</b> {{ task.collaborators.length === 1 ? 'Colaborador' : 'Colaboradores' }}
+                <b>{{ getTask._collaboratorsCount }}</b> {{ getTask._collaboratorsCount === 1 ? 'Colaborador' : 'Colaboradores' }}
               </span>
             </p>
             <p class="main__section__person__block">
               <span class="main__section__person__block__label">Proyecto:</span
               ><br />
               <span class="main__section__person__block__content">
-                <router-link :to="{ name: 'Project', params: { project: project.id } }" class="text-p-light text-500">{{ project.name }}</router-link>
+                <router-link :to="{ name: 'Project', params: { project: getTask.projectId } }" class="text-p-light text-500">{{ getTask.projectName }}</router-link>
               </span>
             </p>
             <div class="main__section__person__block">
               <div class="main__section__card">
                 <div class="main__section__card__date">
-                  <span class="main__section__card__month">{{ meeting.month }}</span>
-                  <span class="main__section__card__day">{{ meeting.day }}</span>
+                  <span class="main__section__card__month">
+                    {{ new Date(getTask.meetingDateInt).toDateString().substr(4, 3) }}
+                  </span>
+                  <span class="main__section__card__day">
+                    {{ new Date(getTask.meetingDateInt).toDateString().substr(8, 2) }}
+                  </span>
                 </div>
                 <router-link
-                  :to="{ name: 'Meeting', params: { meeting: meeting.id } }"
+                  :to="{ name: 'Meeting', params: { meeting: getTask.meetingId } }"
                   class="main__section__card__body"
                 >
                   <span class="main__section__link__text__content">
-                    <span class="text-title">{{ meeting.name }}</span>
+                    <span class="text-title">{{ getTask.meetingName }}</span>
                     <span class="text-content">
-                      {{ meeting.dateInt }} - {{ meeting.dateEnd }}
+                      {{ new Date(getTask.meetingDateInt).getUTCHours() }}:{{ new Date(getTask.meetingDateInt).getUTCMinutes() }}
+                      -
+                      {{ new Date(getTask.meetingDateEnd).getUTCHours() }}:{{ new Date(getTask.meetingDateEnd).getUTCMinutes() }}
                     </span>
                   </span>
                 </router-link>
@@ -43,31 +49,31 @@
                 >Creada:</span
               ><br />
               <span class="main__section__person__block__content">{{
-                new Date(task.createdAt).toLocaleDateString()
+                new Date(getTask.createdAt).toDateString()
               }}</span>
             </p>
             <p
-              v-if="task.createdAt !== task.updatedAt"
+              v-if="getTask.createdAt !== getTask.updatedAt"
               class="main__section__person__block"
             >
               <span class="main__section__person__block__label"
                 >Actualizada:</span
               ><br />
               <span class="main__section__person__block__content">{{
-                new Date(task.updatedAt).toLocaleDateString()
+                new Date(getTask.updatedAt).toDateString()
               }}</span>
             </p>
             <p class="main__section__person__block">
               <span class="main__section__person__block__content"
                 ><i
-                  :class="task.isActive ? 'fas' : 'far'"
+                  :class="getTask.isActive ? 'fas' : 'far'"
                   class="fa-circle"
                 ></i>
-                {{ task.isActive ? "Activa" : "Inactiva" }}</span
+                {{ getTask.isActive ? "Activa" : "Inactiva" }}</span
               ><br />
               <span class="main__section__person__block__content">
-                <i class="fas" :class="task.isLock ? 'fa-lock' : 'fa-lock-open'"></i>
-                {{ task.isLock ? "Cerrada" : "Abierta" }}</span
+                <i class="fas" :class="getTask.isLock ? 'fa-lock' : 'fa-lock-open'"></i>
+                {{ getTask.isLock ? "Cerrada" : "Abierta" }}</span
               >
             </p>
             <div class="main__section__person__block--flex">
@@ -77,7 +83,7 @@
               <button @click="editTask" class="btn-secondary">
                 <i class="fas fa-edit"></i>
               </button>
-              <button @click="checkTask" class="btn-p-dark" v-if="task.isLock">
+              <button @click="checkTask" class="btn-p-dark" v-if="getTask.isLock">
                 <i class="fas fa-check"></i>
               </button>
               <button @click="checkTask" class="btn-outline-gray" v-else>
@@ -92,8 +98,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import { db } from '@/main'
+import { mapActions, mapGetters } from 'vuex'
 
 import TheNavbar from '@/components/TheNavbar'
 
@@ -130,86 +135,18 @@ export default {
         }
       ],
       meeting: {
-        id: '',
+        _id: '',
         name: '',
-        month: '',
-        day: '',
-        dateEnd: '',
-        dateInt: ''
-      },
-      project: '',
-      task: {
-        id: '',
-        name: '',
-        description: '',
-        collaborators: [],
-        meeting: '',
-        project: '',
-        isActive: '',
-        isLock: '',
-        createdAt: '',
-        updatedAt: ''
+        dateInt: '',
+        dateEnd: ''
       }
     }
   },
   created () {
-    this.getTask()
+    this.fetchTask(this.$route.params.task)
   },
   methods: {
-    ...mapActions(['deleteTask', 'updateTask']),
-    async getTask () {
-      try {
-        const data = await db.tasks[this.$route.params.task]
-
-        if (data === undefined) {
-          await this.$router.replace({ name: 'Error' })
-        } else {
-          this.task = {
-            id: await data.id,
-            name: await data.name,
-            description: await data.description,
-            collaborators: await data.collaborators,
-            meeting: await data.meeting,
-            project: await data.project,
-            isActive: await data.isActive,
-            isLock: await data.isLock,
-            createdAt: await data.createdAt,
-            updatedAt: await data.updatedAt
-          }
-
-          await this.getMeeting(this.task.meeting)
-          await this.getProject(this.task.project)
-        }
-      } catch (error) {
-        // eslint-disable-next-line no-useless-return
-        if (error) return
-      }
-    },
-    async getMeeting (id) {
-      try {
-        const data = await db.meetings[id]
-
-        this.meeting = {
-          id: await data.id,
-          name: await data.name,
-          month: new Date(await data.dateInt).toString().split(' ')[1],
-          day: new Date(await data.dateInt).toString().split(' ')[2],
-          dateInt: new Date(await data.dateInt - (1000 * 60 * 60 * 5)).toISOString().substr(11, 5),
-          dateEnd: new Date(await data.dateEnd - (1000 * 60 * 60 * 5)).toISOString().substr(11, 5)
-        }
-      } catch (error) {
-        // eslint-disable-next-line no-useless-return
-        if (error) return
-      }
-    },
-    async getProject (id) {
-      try {
-        this.project = await db.projects[id]
-      } catch (error) {
-        // eslint-disable-next-line no-useless-return
-        if (error) return
-      }
-    },
+    ...mapActions(['fetchTask', 'deleteTask', 'updateTask']),
     async editTask () {
       await this.$router.replace({
         name: 'EditTask',
@@ -217,24 +154,23 @@ export default {
       })
     },
     async checkTask () {
-      this.task.isLock = !this.task.isLock
+      this.getTask.isLock = !this.getTask.isLock
 
       await this.updateTask(this.task)
     },
     async removeTask () {
       if (window.confirm(`Está a punto de borrar un elemento`)) {
-        const task = await this.getTask
+        await this.deleteTask(this.$route.params.task)
 
-        await this.deleteTask(task.id)
-
-        await this.$router.replace({
-          name: 'Tasks'
-        })
+        await this.$router.replace({ name: 'Tasks' })
       }
     }
   },
+  computed: {
+    ...mapGetters(['getTask'])
+  },
   watch: {
-    $route: ['getTask']
+    $route: ['fetchTask']
   }
 }
 </script>
