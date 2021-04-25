da96f61ba666d1844867c675fac14357f395a58c
Óscar Llorente <llorente_oscar@hotmail.com>
2021-04-25 11:02:53 -0500

    Organizando el envío de datos

--- src/views/project/NewProjectMeeting.vue
+++ src/views/project/NewProjectMeeting.vue
@@ -41,8 +41,8 @@
               <select multiple v-model="meeting.collaborators">
                 <option
                   v-for="person in getAllProjectPeople"
-                  :key="person.id"
-                  :value="person.id"
+                  :key="person._id"
+                  :value="person._id"
                 >
                   {{ person.name }} - {{ person.email }}
                 </option>
@@ -94,7 +94,8 @@ export default {
         description: '',
         collaborators: [],
         dateInt: '',
-        dateEnd: ''
+        dateEnd: '',
+        project: this.$route.params.project
       },
       today: new Date().toLocaleString()
     }
@@ -119,26 +120,8 @@ export default {
         setTimeout(() => {
           this.alert.error = false
         }, 4000)
-
-        // eslint-disable-next-line no-useless-return
-        return
       } else {
-        const meeting = {
-          name: await this.meeting.name,
-          description: await this.meeting.description,
-          collaborators: await this.meeting.collaborators,
-          project: this.$route.params.project,
-          dateInt: await this.meeting.dateInt,
-          dateEnd: await this.meeting.dateEnd
-        }
-
-        await this.addMeeting(meeting)
-
-        this.meeting.name = ''
-        this.meeting.description = ''
-        this.meeting.collaborators = ''
-        this.meeting.dateInt = ''
-        this.meeting.dateEnd = ''
+        await this.addMeeting(this.meeting)
 
         await this.$router.replace({
           name: 'ProjectMeetings',
